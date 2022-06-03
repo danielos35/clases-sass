c1d733f6b9e05e30a7c44846297a353952e9c721
Daniel <studiosdopplermusic@gmail.com>
2022-06-03 18:32:26 -0500

    conceptoCSS

---
+++ 02_Notas/02_FuncionamientoCSS.js
@@ -0,0 +1,32 @@
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<FUNCIONAMIENTO DE CSS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+CARGA DE UNA PAGINA WEB
+
+<<<<HTML>>>>
+- Carga de l HTML.
+- Parse HTML,  decodifica el codigo linea por linea.
+- Se crea el DOM (allí se almacena todo el HTML decodificado). 
+
+<<<<CSS>>>>
+- Carga de las ojas de estilos CSS
+- Parse del CSS, decodificación del CSS
+-- Se resuelven las declaraciones CSS en conflicto, mediante un proceso conocido como cascada. 
+-- Se analiza el CSS en valores finales.
+--- Creación de dimensiones (50% en las dimensiones de un telefono es diferente al 50% en una laptop)
+- Se crea el CSSOM (CSS Object Model). es un modelo de objetos similar al DOM.
+
+<<<ARBOL DE RENDERIZADO>>>
+- Unión de DOM y CSSOM 
+
+
+<<Web site rendering>>
+- Pre-renderizado de la pagina.
+
+<<RENDER FINAL>>
+- Vista de todos los elementos de la pagina
+
+
+
+
+*/
