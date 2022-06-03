fba7214fb5a0e9274d945476884c9387595af814
Daniel <studiosdopplermusic@gmail.com>
2022-06-03 18:32:44 -0500

    fundamentos CSS

---
+++ 02_Notas/01_ConceptosFundamentales.js
@@ -0,0 +1,26 @@
+/*
+
+DISEÑO RESPONSIVO.
+- Funcionamiento perfecto para cualquier resolución de pantalla. 
+
+CODIGO MANTENIBLE Y ESCALABLE.
+- Codigo limpio. 
+- Facil de mantener.
+- Reutilizable.
+- Arquitectura CSS. 
+
+RENDIMIENTO WEB 
+- Sitios Web mas eficientes.
+- Minima cantidad de archivos en el documento.
+- Comprimir el codigo.
+- Usar preprocesadores como SASS.
+
+
+
+NOTAS IMPORTANTES
+-Reducir el uso de imagenes mejora el rendimiento web de un sitio.
+-Solo usar las imagenes necesarias para la aplicación.
+-Comprimir las imagenes.
+
+
+*/
