d5311bf24022a919161f281aa3c85757b0922a64
Daniel <studiosdopplermusic@gmail.com>
2022-06-06 19:41:41 -0500

    herencia

---
+++ 02_Notas/05_herencia.js
@@ -0,0 +1,15 @@
+/*
+
+HERENCIA>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+- Es una forma de propagar los valores de elementos padres a hijos
+- No todas las propiedades pueden ser heredadas. 
+- Todos los valores son herdados en px, no en porcentajes.
+- En caso de no heredar un propiedad, la propiedad por defecto del elemento tomará el valor por defecto.
+- Las propiedades relacionadas con el texto se heredan.
+- margenes, paddings no se heredan.
+- podemos usar la palabra inherit (heredar) para forzar una determinada herencia. 
+- Para restablecer una propiedad a su valor inicial y no heredar se usa la propiedad intial.
+
+
+
+*/
