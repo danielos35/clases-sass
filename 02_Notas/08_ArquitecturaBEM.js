6a45e562950d34f90e96a5d2c113a4f26224996c
Daniel <studiosdopplermusic@gmail.com>
2022-06-14 21:25:14 -0500

    bem - 2

---
+++ 02_Notas/08_ArquitecturaBEM.js
@@ -0,0 +1,49 @@
+/*
+Siempre queremos un codigo 
+
+- Limpio.
+- Modular. 
+- Reutilizable.
+- Facil de actualizar si la app necesita crecer. 
+
+
+MODALIDAD
+
+- Pensar 
+-- Pensar en el diseño de nuestra pagina web antes de empezar a escribir el codigo. 
+-- Modularizar y utilizar componentes. 
+--- Los componentes pueden ser reutilizables. 
+--Los componentes deben de ser independientes, para poder utilizarlos neuvamente sin importar en que lugar
+
+- Construir
+-- Despues de tener el diseño claro debemos de empezar a construir la aplicación . 
+-- Podemos basarnos en BEM (modificadore de elemento de bloque)
+-- Block: Componente independiente
+-- Elment: Elemento del bloque, que NO es independiente a este. 
+-- Modifier: Es usado como una bandera para identificar los bloques o elementos uno de otros. (para crear una versión diferente)
+
+
+
+
+- Arquitectura
+-- Crear una arquitectura logica utilizando multiples archivos. 
+-- Estos archivos deberan de tener una logica. 
+-- Patron 7-1
+--- Tenemos 7 carpetas donde ubicamos 7 archivos SASS y luego un archivo SASS principal en el que importamos todos nuestros archivos, esto irán a una unica hojha compilada en CSS
+----base/
+----components/
+----layout/
+----pages/
+----themes/
+----abstracts/
+----vendors/
+
+
+NOTAS. 
+
+
+Diseño atomico: 
+- Leer sobre diseño atomico. 
+- Leer sobre el patron 7-1 
+
+*/
