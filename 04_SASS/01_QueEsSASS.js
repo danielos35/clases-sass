7c19954ae09026edcd656db8f5eb73dd9bc1d150
Daniel <studiosdopplermusic@gmail.com>
2022-06-15 21:33:51 -0500

    Sass

---
+++ 04_SASS/01_QueEsSASS.js
@@ -0,0 +1,19 @@
+/*
+SASS. 
+- Es un preprocesador de CSS, una extensión de CSS para agregar mas potencia y elegancia al lenguage básico.
+- CSS suele volverse desordenado muy rapidamente, cosa que no ocurre con SASS. 
+- SASS Siempre es compilado a CSS. 
+
+
+CARACTERISTICAS DE SASS.
+- Nos permite utilizar VARIABLES, que al igual que en los lenguajes tradicionales podemos reutilizar. 
+- Tenemos ANIDACIONES, permitiendo tener menos codigo. 
+- Tenemos OPERADORES MATEMATICOS para operaciones matematicas dentro de CSS. 
+- PARTIALS e IMPORTACIONES que son de las caracteristicas mas importantes y utiles de SASS, pues nos permite escribir CSS en diferentes archivos y despues importarlos en uno solo.
+- MIXINS Permite escribir piezas reutilizables de codigo CSS
+- FUNCIONES, son bastante similares a los mixins con la difere
+- EXTENSIONES, Se pueden usar para hacer diferenetes selectores hereden declaraciones que son comunes a todos ellos.
+- DIRECTIVAS DE CONTROL, Permite escribir codigo complejo utilizando condiciones y blucles como un lenguaje de programación
+
+
+*/
