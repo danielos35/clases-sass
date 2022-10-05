c78afd26ebccf53352d23c85cd32d7352457a1c9
Daniel <studiosdopplermusic@gmail.com>
2022-10-04 22:42:44 -0500

    responsive intro

---
+++ 05_responsibe/01_principios.js
@@ -0,0 +1,19 @@
+/*
+
+Responsive:
+
+Ajuste de la pantalla para cualquier dispositivo
+
+PRINCIPIOS
+-- FLUID LAYAOUTS
+--- Permite que la pantalla se adapte al ancho de la ventana grafica actual
+
+-- RESPONSIVE UNITS
+--- Usar rem en lugar de px, para poder escalar en caso de ser necesario
+
+-- FLEXIBLE IMAGES
+--- Ajustar las imagenes
+
+-- MEDIA QUERIES
+--- Crear versiones de un sitio web para diferentes dispositvos
+*/
