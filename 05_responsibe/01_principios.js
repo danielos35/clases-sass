3ca66a51edc3780f06c085f68b1cf9450ff86819
Daniel <studiosdopplermusic@gmail.com>
2022-10-05 23:15:05 -0500

    Types | Responsive Desing

--- 05_responsibe/01_principios.js
+++ 05_responsibe/01_principios.js
@@ -7,6 +7,10 @@ Ajuste de la pantalla para cualquier dispositivo
 PRINCIPIOS
 -- FLUID LAYAOUTS
 --- Permite que la pantalla se adapte al ancho de la ventana grafica actual
+---- FLOAT LAYOUTS (es una tecnologia vieja de css)
+---- FLEXBOX (forma moderna de diseñar elementos)
+---- CSS GRID ()
+
 
 -- RESPONSIVE UNITS
 --- Usar rem en lugar de px, para poder escalar en caso de ser necesario
@@ -16,4 +20,5 @@ PRINCIPIOS
 
 -- MEDIA QUERIES
 --- Crear versiones de un sitio web para diferentes dispositvos
+
 */
