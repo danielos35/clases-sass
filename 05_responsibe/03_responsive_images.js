98fcca7c4a174e9fda89564bcc99c6c9ec6327d7
Daniel <studiosdopplermusic@gmail.com>
2022-10-09 23:45:16 -0500

    responsive images

---
+++ 05_responsibe/03_responsive_images.js
@@ -0,0 +1,10 @@
+/*
+Imagenes responsive
+-- Descargar las imagenes deacuerdo al tamaño de su dispositivo
+
+CUANDO USAR?
+-- CAmbios de resolución
+-- Dependiendo la resolución del dispositivo.
+-- Art direction (dependiendo la distribución de la pantalla)
+
+*/
