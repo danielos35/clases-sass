905c1469dc8b5da251305c215249ba725d7aacfb
Daniel <studiosdopplermusic@gmail.com>
2022-10-19 18:47:46 -0500

    Grid items

---
+++ 10grid/01_WTF_is_Grid.js
@@ -0,0 +1,53 @@
+/*
+
+GRID
+
+-  Grid nos permite realizar desings mas complejos.
+
+
+CONTAINER 
+
+- .... grid-template... 
+
+-- grid-template-rows
+-- grid-template-columns
+-- grid-template-areas
+
+- .... grid-template...
+
+-- grid-row-gap
+-- grid-column-gap
+
+-- justify-items
+-- align-items
+-- justify-content
+-- align-content
+
+-- grid-auto-rows
+-- grid-auto-columns
+-- grid-auto-flow
+
+
+ITEM
+
+-grid-area
+
+- .... grid-row...
+
+-- grid-row-start
+-- grid-row-end
+
+- .... grid-column...
+
+-- grid-column-start
+-- grid-column-end
+
+-- justify-self
+-- align-self
+
+-- order
+
+
+
+
+*/ 
