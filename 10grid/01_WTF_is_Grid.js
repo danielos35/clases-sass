ff64880b28dd41f4be24b3ccf3375f7749f5d9e8
Daniel <studiosdopplermusic@gmail.com>
2022-10-21 20:04:47 -0500

    data

--- 10grid/01_WTF_is_Grid.js
+++ 10grid/01_WTF_is_Grid.js
@@ -32,15 +32,20 @@ ITEM
 
 -grid-area
 
+--- Resumen todos los elementos siguientes en una sola posición
+---- grid-area:1/3/2/4; (Primeras dos posiciones filas, segunda columnas)
+
 - .... grid-row...
 
--- grid-row-start
--- grid-row-end
-
+-- grid-row-start: orden en el que se ubicará el elemento (grid-row-start:2;)
+-- grid-row-end: ubicación final del elemento (grid-row-end:3;)
+    (resumir en una sola linea grid-row:2/3;)
+    
 - .... grid-column...
-
--- grid-column-start
--- grid-column-end
+    
+-- grid-column-start: orden de la columna en la que se ubicará (grid-column-start:3;)
+-- grid-column-end: ubicación final del elemento (grid-column-end:2;)
+    (resumir en una sola linea grid-column:3/2;)
 
 -- justify-self
 -- align-self
